60ccfff4b4b661cc586beaff3fa9cd2f33cb0cf3
Vincent <vincent.debuisson@gmail.com>
2021-12-21 09:30:53 +0100

    first commit

---
+++ src/main/java/com/example/courstestunitaire/CoursTestUnitaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.courstestunitaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursTestUnitaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursTestUnitaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courstestunitaire/CoursTestUnitaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courstestunitaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursTestUnitaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
